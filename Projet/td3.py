24ddb83beec088623feed1441d12cef02970169e
Gael FERREIRA RODRIGUEZ <gael.ferreira-rodriguez@ens.uvsq.fr>
2022-10-14 10:42:12 +0200

    #

--- Projet/td3.py
+++ Projet/td3.py
@@ -1,3 +1,5 @@
+import time, calendar
+
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
     return temps[0]*86400 + temps[1]*3600 + temps[2]*60 + temps[3]
@@ -63,52 +65,94 @@ def proportionTemps(temps,proportion):
     return secondeEnTemps(temps_proportion_seconde)
     
 #afficheTemps(proportionTemps((2,0,36,0),0.2))
-
+'''
 def tempsEnDate(temps):
     année = 1970 + (temps[0] // 365)
-    mois = (temps[0] % année) // 31
+    mois = (temps[0] % 365) // 31
     jour = 0
     if année % 4 == 0 and not année % 100 == 0:
         if mois == 2:
-            mois = (temps[0] % année) // 29
+            mois = (temps[0] % 366) // 29
             jour = temps[0] % mois
         elif (mois % 2 == 0) or mois == 7:
-            mois = (temps[0] % année) // 30
+            mois = (temps[0] % 366) // 31
             jour = temps[0] % mois
         else:
-            mois = (temps[0] % année) // 30
+            mois = (temps[0] % 366) // 30
             jour = temps[0] % mois
     else:
         if mois == 2:
-            mois = (temps[0] % année) // 28
+            mois = (temps[0] % 365) // 28
             jour = temps[0] % mois
         elif (mois % 2 == 0) or mois == 7:
-            mois = (temps[0] % année) // 30
+            mois = (temps[0] % 365) // 31
             jour = temps[0] % mois
         else:
-            mois = (temps[0] % année) // 30
+            mois = (temps[0] % 365) // 30
             jour = temps[0] % mois
-    
     temps_date = (année, mois, jour, temps[1], temps[2], temps[3])
     return temps_date
-
+'''
+'''
 def afficheDate(date = -1):
     mot_pluriel = "s"
-    Mois = ["", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"]
+    Mois = ["", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
     Temps = ["année", "mois", "jour", "heure", "minute", "seconde"]
     for i in range(6):
         if i == 1:
-            print(temps[i], , end=" ")
-        elif temps[i] == 0:
-            print("", end=" ")
-        elif temps[i] > 1:
-            print(temps[i], Temps[i] + mot_pluriel, end=" ")
+            print(Mois[date[i]], end=" ")
+        elif i == 0:
+            print(date[i], end=" ")
         else:
-            print(temps[i], Temps[i], end=" ")
+            if date[i] == 0:
+                print("", end=" ")
+            elif date[i] > 1:
+                print(date[i], Temps[i] + mot_pluriel, end=" ")
+            else:
+                print(date[i], Temps[i], end=" ")
     
+'''
+def tempsEnDate(temps):
+    année = 1970 + (temps[0] // 365)
+    jour = temps[0] % 365
+    temps_date = (année, jour, temps[1], temps[2], temps[3])
+    return temps_date
+ 
+def afficheDate(date = -1):
+    mot_pluriel = "s"
+    Temps = ["année", "jour", "heure", "minute", "seconde"]
+    for i in range(5):
+        if date[i] == 0:
+            print("", end=" ")
+        elif date[i] > 1:
+            print(date[i], Temps[i] + mot_pluriel, end=" ")
+        else:
+            print(date[i], Temps[i], end=" ")
     
+ 
 temps = secondeEnTemps(1000000000)
 afficheTemps(temps)
 tempsEnDate(temps)
 afficheDate(tempsEnDate(temps))
-afficheDate()
+#afficheDate()
+
+def nombreBisextile(jour):
+    année = 1970 
+    nbre_année_bisextile = 0
+    for i in range(jour // 365):
+        if année % 4 == 0 and (not année % 100 == 0):
+            nbre_année_bisextile += 1
+            année += 1
+        
+    return nbre_année_bisextile
+
+def tempsEnDateBisextile(temps):
+    année = 1970 + (temps[0] // 365)
+    jour = (temps[0] % 365) 
+    jour_bissextile = jour + nombreBisextile(jour)
+    temps_date = (année, jour, temps[1], temps[2], temps[3])
+    return temps_date
+   
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDateBisextile(temps))
