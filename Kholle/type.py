b0869a6726addab8410a52b1ae6f2c84783914d8
Gael FERREIRA RODRIGUEZ <gael.ferreira-rodriguez@ens.uvsq.fr>
2022-10-27 19:44:45 +0200

    wesh

---
+++ Kholle/type.py
@@ -0,0 +1,104 @@
+import random as rd
+
+def Ordre():
+    a, b = int(input("Saisir une valeur a")), int(input("Saisir une valeur b"))
+    if a < b:
+        a, b = b, a
+
+    return a, b
+
+def Diviseur():
+    a = "Ne divise pas"
+    nbre_aleatoire = rd.randint(10, 99)
+    nbre_utilisateur = int(input("Saisir un nombre à un chiffre : "))
+    if nbre_utilisateur > 1 and (nbre_aleatoire % nbre_utilisateur) == 0:
+        a = "Diviseur"
+
+    return a
+   
+def Permutation(var1, var2):
+    if var1 * var2 > 0:
+        var1, var2 = var2, var1
+    else:
+        var1, var2 = var1 ** var2, var1 * var2
+    
+    return var1, var2
+
+def PGCD(a, b):
+    pgcd = 1
+    i = 1
+    if a >= b:
+        while i < a:
+            if (a % i == 0) and (b % i == 0):
+                pgcd = i
+            i += 1
+    else:
+        while i < b:
+            if (a % i == 0) and (b % i == 0):
+                pgcd = i
+            i += 1
+
+    return pgcd
+
+
+def Somme():
+    x = int(input("Saisir un entier :\n"))
+    somme = 0
+    nombre = 0
+    nombre_sup100 = 0
+    while x >= 0:
+        somme += x
+        nombre += 1
+        if x > 100:
+            nombre_sup100 += 1
+        x = int(input("Saisir un entier :\n"))
+
+    return somme, nombre, nombre_sup100
+
+def Divisible_par2():
+    '''Calcul le nombre de fois de suite que l'entier donné est divisible par deux'''
+    x = int(input("Saisir un entier :\n"))
+    divisible = 0
+    while x % 2 == 0:
+        divisible += 1
+        x = int(x / 2) 
+
+    return divisible
+
+def Diviseurs():
+    '''Diviseur propre d'un entier strictement supérieur à 1'''
+    x = int(input("Saisir un entier strictement supérieur à 1 :\n"))
+    diviseur_liste = [] #liste de tous les divisuers du nombre donné
+    verif = 0 #permettra de vérifier si le nombre possède un diviseur, et dans le cas contraire pouvoir afficher qu'il est un nombre premier
+
+    while x < 1: #vérifie que le nombre est bien strictement supérieur à 1
+        x = int(input("Veuillez recommencer en saisissant un entier strictement supérieur à 1 :\n"))
+
+    for i in range(2, x): 
+        if x % i == 0:
+            diviseur_liste.append(i)
+            verif += 1
+
+    if verif >= 1:
+        print("Les diviseurs de", x, "sont :", Liste_into_number(diviseur_liste))
+    else:
+        print(x, "est donc une nombre premier")
+
+def Liste_into_number(x):
+    '''Transforme les données d'une liste ou d'un tuple en une chaîne de caractère espacé'''
+    a = ""
+    for i in x:
+        a += str(i) + " "
+
+    return a
+
+def hauteurParcourue(nb_marche, h_marche):
+    metre = 7 * (5 * 2 * ((h_marche * (10 ** -2)) * nb_marche))
+    print("Pour", nb_marche, "marches de", h_marche, "cm, il parcourt", metre,"m par semaine.")
+
+def Longueur():
+    nb_marche = int(input("Combien de marches ?\n"))
+    h_marche = int(input("Quelle hauteur pour les marches ?\n"))
+
+    return hauteurParcourue(nb_marche, h_marche)
+
--- Projet/td3.py
+++ Projet/td3.py
@@ -29,10 +29,6 @@ def afficheTemps(temps):
         else:
             print(temps[i], Temps[i], end=" ")
 
-    
-#afficheTemps((1,0,14,23)) 
-
-#Demande le temps
 
 def demandeTemps():
     Jour = int(input("nombre de jour(s) : "))
@@ -48,8 +44,6 @@ def demandeTemps():
     
     return (Jour, Heure, Minute, Seconde)
 
-#afficheTemps(demandeTemps())
-
 
 def sommeTemps(temps1,temps2):
     temps1_seconde, temps2_seconde = tempsEnSeconde(temps1), tempsEnSeconde(temps2)
@@ -57,61 +51,11 @@ def sommeTemps(temps1,temps2):
 
     return secondeEnTemps(temps_total_seconde)
 
-#sommeTemps((2,3,4,25),(5,22,57,1))
-
 def proportionTemps(temps,proportion):
     temps_proportion_seconde = tempsEnSeconde(temps) * proportion
 
     return secondeEnTemps(temps_proportion_seconde)
-    
-#afficheTemps(proportionTemps((2,0,36,0),0.2))
-'''
-def tempsEnDate(temps):
-    année = 1970 + (temps[0] // 365)
-    mois = (temps[0] % 365) // 31
-    jour = 0
-    if année % 4 == 0 and not année % 100 == 0:
-        if mois == 2:
-            mois = (temps[0] % 366) // 29
-            jour = temps[0] % mois
-        elif (mois % 2 == 0) or mois == 7:
-            mois = (temps[0] % 366) // 31
-            jour = temps[0] % mois
-        else:
-            mois = (temps[0] % 366) // 30
-            jour = temps[0] % mois
-    else:
-        if mois == 2:
-            mois = (temps[0] % 365) // 28
-            jour = temps[0] % mois
-        elif (mois % 2 == 0) or mois == 7:
-            mois = (temps[0] % 365) // 31
-            jour = temps[0] % mois
-        else:
-            mois = (temps[0] % 365) // 30
-            jour = temps[0] % mois
-    temps_date = (année, mois, jour, temps[1], temps[2], temps[3])
-    return temps_date
-'''
-'''
-def afficheDate(date = -1):
-    mot_pluriel = "s"
-    Mois = ["", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
-    Temps = ["année", "mois", "jour", "heure", "minute", "seconde"]
-    for i in range(6):
-        if i == 1:
-            print(Mois[date[i]], end=" ")
-        elif i == 0:
-            print(date[i], end=" ")
-        else:
-            if date[i] == 0:
-                print("", end=" ")
-            elif date[i] > 1:
-                print(date[i], Temps[i] + mot_pluriel, end=" ")
-            else:
-                print(date[i], Temps[i], end=" ")
-    
-'''
+
 def tempsEnDate(temps):
     année = 1970 + (temps[0] // 365)
     jour = temps[0] % 365
@@ -150,9 +94,10 @@ def tempsEnDateBisextile(temps):
     année = 1970 + (temps[0] // 365)
     jour = (temps[0] % 365) 
     jour_bissextile = jour + nombreBisextile(jour)
-    temps_date = (année, jour, temps[1], temps[2], temps[3])
+    temps_date = (année, jour_bissextile, temps[1], temps[2], temps[3])
     return temps_date
    
-temps = secondeEnTemps(1000000000)
-afficheTemps(temps)
-afficheDate(tempsEnDateBisextile(temps))
+#temps = secondeEnTemps(1000000000)
+#afficheTemps(temps)
+#nombreBisextile(11574)
+#afficheDate(tempsEnDateBisextile(temps))
